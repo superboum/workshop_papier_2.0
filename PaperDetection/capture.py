96ce0b3cff853d918d7b38f796e5e19629d3d65b
Quentin Dufour <quentin@dufour.tk>
2016-10-08 20:27:50 +0200

    Etalonnage automatique

--- PaperDetection/capture.py
+++ PaperDetection/capture.py
@@ -6,11 +6,11 @@ import time
 
 ### A CONFIGURER !
 
-DETECT_RED = 255
-DETECT_GREEN = 166
-DETECT_BLUE = 132
 DELTA = 50
 SIZE = 50
+DETECT_RED = 0
+DETECT_BLUE = 0
+DETECT_GREEN = 0
 
 ### CODE
 
@@ -19,11 +19,31 @@ cap = cv2.VideoCapture(0)
 #### Etalonnage
 _, frame = cap.read()
 height, width, channels = frame.shape
+etalonnage_top_left = (width/2 - SIZE, height / 2 - SIZE)
+etalonnage_bottom_right = (width/2 + SIZE, height / 2 + SIZE)
 
 start_time = time.time()
-while start_time+10 > time.time():
+while start_time+4 > time.time():
     _, frame = cap.read()
-    cv2.rectangle(frame, (width/2 - SIZE, height / 2 - SIZE), (width/2 + SIZE, height / 2 + SIZE), (255,0,0), 5)
+    cv2.rectangle(frame, etalonnage_top_left, etalonnage_bottom_right, (255, 0, 0), 5)
+    cv2.imshow('frame',frame)
+    k = cv2.waitKey(5) & 0xFF
+
+count_pix = 0
+for i in range (etalonnage_top_left[0]+1, etalonnage_bottom_right[0]):
+    for j in range(etalonnage_top_left[1]+1, etalonnage_bottom_right[1]):
+        DETECT_BLUE += frame[j][i][0]
+        DETECT_GREEN += frame[j][i][1]
+        DETECT_RED += frame[j][i][2]
+        count_pix += 1
+
+DETECT_BLUE /= count_pix
+DETECT_GREEN /= count_pix
+DETECT_RED /= count_pix
+
+while start_time+6 > time.time():
+    _, frame = cap.read()
+    cv2.rectangle(frame, etalonnage_top_left, etalonnage_bottom_right, (DETECT_BLUE, DETECT_GREEN, DETECT_RED), 10)
     cv2.imshow('frame',frame)
     k = cv2.waitKey(5) & 0xFF
 
